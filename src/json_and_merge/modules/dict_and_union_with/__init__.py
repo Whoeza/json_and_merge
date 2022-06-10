3677ebc6f9af932a5418f8e3f36d39b2f558373f
Whoeza <84461456+Whoeza@users.noreply.github.com>
2022-06-10 13:42:18 +0200

    removing old deps

--- src/json_and_merge/modules/dict_and_union_with/__init__.py
+++
@@ -1,8 +0,0 @@
-from .dict_union_with import (
-    union_with,
-    union_all_with,
-    union_with_all)
-
-__all__ = ["union_with",
-           "union_all_with",
-           "union_with_all"]
--- src/json_and_merge/modules/json_and_validation/__init__.py
+++
@@ -1,7 +0,0 @@
-from .json_validation import (
-    is_valid_json_file,
-    is_valid_json_string
-)
-
-__all__ = ["is_valid_json_file",
-           "is_valid_json_string"]
